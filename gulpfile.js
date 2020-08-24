dbf9203e8833d4f8194b160728f6ca910c480b52
Vijaya Anand <vanand@apttus.com>
2020-08-24 13:55:39 +0530

    upgraded to angular 10

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
 var gulp = require('gulp'),
-  path = require('path'),
-  ngc = require('@angular/compiler-cli/src/main').main,
-  rollup = require('gulp-rollup'),
-  rename = require('gulp-rename'),
-  del = require('del'),
-  runSequence = require('run-sequence'),
-  inlineResources = require('./tools/gulp/inline-resources');
+    path = require('path'),
+    ngc = require('@angular/compiler-cli/src/main').main,
+    rollup = require('gulp-rollup'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    inlineResources = require('./tools/gulp/inline-resources');
 
 const rootFolder = path.join(__dirname);
 const srcFolder = path.join(rootFolder, 'src');
@@ -15,14 +15,16 @@ const buildFolder = path.join(rootFolder, 'build');
 const distFolder = path.join(rootFolder, 'dist');
 const bundlesFolder = path.join(rootFolder, 'dist/bundles');
 
+var ngFsUtils = require('@angular/compiler-cli/src/ngtsc/file_system');
+
 /**
  * 1. Delete /dist folder
  */
 gulp.task('clean:dist', function () {
 
-  // Delete contents but not dist folder to avoid broken npm links
-  // when dist directory is removed while npm link references it.
-  return deleteFolders([distFolder + '/**', '!' + distFolder]);
+    // Delete contents but not dist folder to avoid broken npm links
+    // when dist directory is removed while npm link references it.
+    return deleteFolders([distFolder + '/**', '!' + distFolder]);
 });
 
 /**
@@ -31,8 +33,8 @@ gulp.task('clean:dist', function () {
  *    when copying to /.tmp.
  */
 gulp.task('copy:source', function () {
-  return gulp.src([`${srcFolder}/**/*`, `!${srcFolder}/node_modules`])
-    .pipe(gulp.dest(tmpFolder));
+    return gulp.src([`${srcFolder}/**/*`, `!${srcFolder}/node_modules`])
+               .pipe(gulp.dest(tmpFolder));
 });
 
 /**
@@ -40,8 +42,8 @@ gulp.task('copy:source', function () {
  *    We do this on the /.tmp folder to avoid editing the original /src files
  */
 gulp.task('inline-resources', function () {
-  return Promise.resolve()
-    .then(() => inlineResources(tmpFolder));
+    return Promise.resolve()
+                  .then(() => inlineResources(tmpFolder));
 });
 
 
@@ -50,15 +52,11 @@ gulp.task('inline-resources', function () {
  *    compiled modules to the /build folder.
  */
 gulp.task('ngc', function () {
-  return ngc({
-    project: `${tmpFolder}/tsconfig.es5.json`
-  })
-    .then((exitCode) => {
-      if (exitCode === 1) {
-        // This error is caught in the 'compile' task by the runSequence method callback
-        // so that when ngc fails to compile, the whole compile process stops running
-        throw new Error('ngc compilation failed');
-      }
+    ngFsUtils.setFileSystem(new ngFsUtils.NodeJSFileSystem());
+    return ngc(['-p', `${tmpFolder}/tsconfig.es5.json`], (error) => {
+        if (error) {
+            throw new Error('ngc compilation failed: ' + error);
+        }
     });
 });
 
@@ -67,33 +65,33 @@ gulp.task('ngc', function () {
  *    generated file into the /dist folder
  */
 gulp.task('rollup:fesm', function () {
-  return gulp.src(`${buildFolder}/**/*.js`)
-  // transform the files here.
-    .pipe(rollup({
+    return gulp.src(`${buildFolder}/**/*.js`)
+               // transform the files here.
+               .pipe(rollup({
 
-      // Bundle's entry point
-      // See "input" in https://rollupjs.org/#core-functionality
-      input: `${buildFolder}/index.js`,
+                                // Bundle's entry point
+                                // See "input" in https://rollupjs.org/#core-functionality
+                                input: `${buildFolder}/index.js`,
 
-      // Allow mixing of hypothetical and actual files. "Actual" files can be files
-      // accessed by Rollup or produced by plugins further down the chain.
-      // This prevents errors like: 'path/file' does not exist in the hypothetical file system
-      // when subdirectories are used in the `src` directory.
-      allowRealFiles: true,
+                                // Allow mixing of hypothetical and actual files. "Actual" files can be files
+                                // accessed by Rollup or produced by plugins further down the chain.
+                                // This prevents errors like: 'path/file' does not exist in the hypothetical file system
+                                // when subdirectories are used in the `src` directory.
+                                allowRealFiles: true,
 
-      // A list of IDs of modules that should remain external to the bundle
-      // See "external" in https://rollupjs.org/#core-functionality
-      external: [
-        '@angular/core',
-        '@angular/common',
-        '@angular/platform-browser'
-      ],
+                                // A list of IDs of modules that should remain external to the bundle
+                                // See "external" in https://rollupjs.org/#core-functionality
+                                external: [
+                                    '@angular/core',
+                                    '@angular/common',
+                                    '@angular/platform-browser'
+                                ],
 
-      // Format of generated bundle
-      // See "format" in https://rollupjs.org/#core-functionality
-      format: 'es'
-    }))
-    .pipe(gulp.dest(distFolder));
+                                // Format of generated bundle
+                                // See "format" in https://rollupjs.org/#core-functionality
+                                format: 'es'
+                            }))
+               .pipe(gulp.dest(distFolder));
 });
 
 /**
@@ -101,49 +99,49 @@ gulp.task('rollup:fesm', function () {
  *    generated file into the /dist folder
  */
 gulp.task('rollup:umd', function () {
-  return gulp.src(`${buildFolder}/**/*.js`)
-  // transform the files here.
-    .pipe(rollup({
+    return gulp.src(`${buildFolder}/**/*.js`)
+               // transform the files here.
+               .pipe(rollup({
 
-      // Bundle's entry point
-      // See "input" in https://rollupjs.org/#core-functionality
-      input: `${buildFolder}/index.js`,
+                                // Bundle's entry point
+                                // See "input" in https://rollupjs.org/#core-functionality
+                                input: `${buildFolder}/index.js`,
 
-      // Allow mixing of hypothetical and actual files. "Actual" files can be files
-      // accessed by Rollup or produced by plugins further down the chain.
-      // This prevents errors like: 'path/file' does not exist in the hypothetical file system
-      // when subdirectories are used in the `src` directory.
-      allowRealFiles: true,
+                                // Allow mixing of hypothetical and actual files. "Actual" files can be files
+                                // accessed by Rollup or produced by plugins further down the chain.
+                                // This prevents errors like: 'path/file' does not exist in the hypothetical file system
+                                // when subdirectories are used in the `src` directory.
+                                allowRealFiles: true,
 
-      // A list of IDs of modules that should remain external to the bundle
-      // See "external" in https://rollupjs.org/#core-functionality
-      external: [
-        '@angular/core',
-        '@angular/common',
-        '@angular/platform-browser'
-      ],
+                                // A list of IDs of modules that should remain external to the bundle
+                                // See "external" in https://rollupjs.org/#core-functionality
+                                external: [
+                                    '@angular/core',
+                                    '@angular/common',
+                                    '@angular/platform-browser'
+                                ],
 
-      // Format of generated bundle
-      // See "format" in https://rollupjs.org/#core-functionality
-      format: 'umd',
+                                // Format of generated bundle
+                                // See "format" in https://rollupjs.org/#core-functionality
+                                format: 'umd',
 
-      // Export mode to use
-      // See "exports" in https://rollupjs.org/#danger-zone
-      exports: 'named',
+                                // Export mode to use
+                                // See "exports" in https://rollupjs.org/#danger-zone
+                                exports: 'named',
 
-      // The name to use for the module for UMD/IIFE bundles
-      // (required for bundles with exports)
-      // See "name" in https://rollupjs.org/#core-functionality
-      name: 'ngx-gallery',
+                                // The name to use for the module for UMD/IIFE bundles
+                                // (required for bundles with exports)
+                                // See "name" in https://rollupjs.org/#core-functionality
+                                name: 'ngx-gallery',
 
-      // See "globals" in https://rollupjs.org/#core-functionality
-      globals: {
-        typescript: 'ts'
-      }
+                                // See "globals" in https://rollupjs.org/#core-functionality
+                                globals: {
+                                    typescript: 'ts'
+                                }
 
-    }))
-    .pipe(rename('ngx-gallery.umd.js'))
-    .pipe(gulp.dest(bundlesFolder));
+                            }))
+               .pipe(rename('ngx-gallery.umd.js'))
+               .pipe(gulp.dest(bundlesFolder));
 });
 
 /**
@@ -152,68 +150,68 @@ gulp.task('rollup:umd', function () {
  *    on step 5.
  */
 gulp.task('copy:build', function () {
-  return gulp.src([`${buildFolder}/**/*`, `!${buildFolder}/**/*.js`])
-    .pipe(gulp.dest(distFolder));
+    return gulp.src([`${buildFolder}/**/*`, `!${buildFolder}/**/*.js`])
+               .pipe(gulp.dest(distFolder));
 });
 
 /**
  * 8. Copy package.json from /src to /dist
  */
 gulp.task('copy:manifest', function () {
-  return gulp.src([`${srcFolder}/package.json`])
-    .pipe(gulp.dest(distFolder));
+    return gulp.src([`${srcFolder}/package.json`])
+               .pipe(gulp.dest(distFolder));
 });
 
 /**
  * 9. Copy README.md from / to /dist
  */
 gulp.task('copy:readme', function () {
-  return gulp.src([path.join(rootFolder, 'README.md')])
-    .pipe(gulp.dest(distFolder));
+    return gulp.src([path.join(rootFolder, 'README.md')])
+               .pipe(gulp.dest(distFolder));
 });
 
 /**
  * 10. Delete /.tmp folder
  */
 gulp.task('clean:tmp', function () {
-  return deleteFolders([tmpFolder]);
+    return deleteFolders([tmpFolder]);
 });
 
 /**
  * 11. Delete /build folder
  */
 gulp.task('clean:build', function () {
-  return deleteFolders([buildFolder]);
+    return deleteFolders([buildFolder]);
 });
 
 gulp.task('compile', function () {
-  runSequence(
-    'clean:dist',
-    'copy:source',
-    'inline-resources',
-    'ngc',
-    'rollup:fesm',
-    'rollup:umd',
-    'copy:build',
-    'copy:manifest',
-    'copy:readme',
-    'clean:build',
-    'clean:tmp',
-    function (err) {
-      if (err) {
-        console.log('ERROR:', err.message);
-        deleteFolders([distFolder, tmpFolder, buildFolder]);
-      } else {
-        console.log('Compilation finished succesfully');
-      }
-    });
+    runSequence(
+        'clean:dist',
+        'copy:source',
+        'inline-resources',
+        'ngc',
+        'rollup:fesm',
+        'rollup:umd',
+        'copy:build',
+        'copy:manifest',
+        'copy:readme',
+        'clean:build',
+        'clean:tmp',
+        function (err) {
+            if (err) {
+                console.log('ERROR:', err.message);
+                deleteFolders([distFolder, tmpFolder, buildFolder]);
+            } else {
+                console.log('Compilation finished succesfully');
+            }
+        });
 });
 
 /**
  * Watch for any change in the /src folder and compile files
  */
 gulp.task('watch', function () {
-  gulp.watch(`${srcFolder}/**/*`, ['compile']);
+    gulp.watch(`${srcFolder}/**/*`, ['compile']);
 });
 
 gulp.task('clean', ['clean:dist', 'clean:tmp', 'clean:build']);
@@ -226,5 +224,5 @@ gulp.task('default', ['build:watch']);
  * Deletes the specified folder
  */
 function deleteFolders(folders) {
-  return del(folders);
+    return del(folders);
 }
